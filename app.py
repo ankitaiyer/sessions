a7f26d8739d589225f58b60d22a12f4cd720af8a
Student <user@chromebox-001.(none)>
2014-02-26 18:15:00 -0800

    Post user's comments

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, session, url_for, flash
 import model
+from urlparse import urlparse
 
 app = Flask(__name__)
 app.secret_key = "shhhhthisisasecret"
@@ -37,6 +38,24 @@ def session_clear():
 #    session.clear(session["hackbright"])
     session.clear()
     return redirect(url_for("index"))
+
+@app.route("/user/<username>")
+def show_user_profile(username):
+    rows = model.get_all_posts(username)
+    return render_template("wall.html", rows=rows, username=username)
+
+@app.route("/user/<username>", methods=["POST"])
+def post_to_wall(username):
+    print "****************************************"
+    # print "USERNAME", username
+    owner_id = model.get_user_by_name(username)
+    print "OWNER_ID", owner_id
+    author_id = session.get("user_id")
+    wall_post = request.form.get("wall_post")
+    print "AUTHOR ID", author_id
+    print "WALL POST", wall_post
+    model.submit_post(owner_id,author_id,wall_post)
+    return redirect(url_for("show_user_profile", username=username))
 
 if __name__ == "__main__":
     app.run(debug = True)
--- model.py
+++ model.py
@@ -1,4 +1,5 @@
 import sqlite3
+import datetime
 
 ADMIN_USER="hackbright"
 ADMIN_PASSWORD=5980025637247534551
@@ -11,15 +12,33 @@ def connect_to_db():
     CONN = sqlite3.connect("thewall.db")
     DB = CONN.cursor()
 
-def getContent_user_by_name(username):
-    query = """SELECT users.id, wall_posts.content  
+def get_user_by_name(username):
+    connect_to_db()
+    query = """SELECT id FROM users WHERE username = ?"""
+    DB.execute(query, (username, ))
+    row = DB.fetchone()
+    print "THIS IS THE ROW", row[0]
+    return row[0]
+
+# get_all_posts(username):
+def get_all_posts(username):
+    connect_to_db()
+    query = """SELECT users.username, wall_posts.content, wall_posts.created_at  
     FROM users
     JOIN wall_posts ON (users.id = wall_posts.owner_id)
     WHERE username == (?)"""
     DB.execute(query, (username, ))
-    row = DB.fetchall()
-    print row
+    rows = DB.fetchall()
+    return rows
 
+def submit_post(owner_id,author_id,wall_post):
+    connect_to_db()
+    now = datetime.datetime.utcnow()
+    print "TIMESTAMP", now
+    query = """INSERT INTO wall_posts values (null, ?, ?, ?, ?)"""
+    DB.execute(query, (owner_id, author_id, now, wall_post))
+    CONN.commit()
+   
 
 def authenticate(username, password):
     connect_to_db()
