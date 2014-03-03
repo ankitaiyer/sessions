e27b3ba0dddf095ee01235dc7eb7e40742af3a96
Iyer <ankitai516@MTVL1253bb348.local>
2014-03-02 21:56:17 -0800

    create user, register and show user profile done. Extra credits not done

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, session, url_for, flash
 import model
-from urlparse import urlparse
+
+
 
 app = Flask(__name__)
 app.secret_key = "shhhhthisisasecret"
@@ -21,34 +22,55 @@ def process_login():
  #   return user_id
     if user_id != None:
         flash("User authenticated!")
-        #session['username'] = username
         session['user_id'] = user_id
     else:
         flash("Password incorrect, there may be a ferret stampede in progress.")
 
-    return redirect(url_for("index"))
-#    return render_template("login.html")
+    #return redirect(url_for("index", username=username))
+    return redirect(url_for("show_user_profile", username=username))
 
 @app.route("/register")
 def register():
-    return render_template("register.html")
+    if session.get("user_id"):
+        username = model.get_username_by_userid(session.get("user_id"))
+        return redirect(url_for("show_user_profile", username=username))
+    else:    
+        return render_template("register.html")
 
-@app.route("/clear_session")
-def session_clear():
-#    session.clear(session["hackbright"])
-    session.clear()
-    return redirect(url_for("index"))
+
+@app.route("/register", methods=["POST"])
+def create_account():
+    # if session.get("user_id"):
+    #     username = model.get_username_by_userid(session.get("user_id"))
+    #     return redirect(url_for("show_user_profile", username=username))
+    # else: 
+        print "CREATING ACCOUNT"
+        username = request.form.get("username")
+        password = request.form.get("password")
+        print "USERMAME", username
+        print "PASSWORD", password
+        model.create_account(username, password)
+
+        #return render_template("register.html", username=username)
+        return redirect(url_for("process_login"))
+
 
 @app.route("/user/<username>")
 def show_user_profile(username):
     rows = model.get_all_posts(username)
-    return render_template("wall.html", rows=rows, username=username)
+    message = "Sorry!" + session.get(user_id) + "You don't have any Posts"
+    if rows == None:
+        flash(message)
+        return redirect(url_for("index"))
+    else:
+        user_id = session.get("user_id")
+        #author_name = model.get_username_by_author_id(session.get("user_id"))
+        return render_template("wall.html", rows=rows, username=username, 
+                                )
 
 @app.route("/user/<username>", methods=["POST"])
 def post_to_wall(username):
-    print "****************************************"
-    # print "USERNAME", username
-    owner_id = model.get_user_by_name(username)
+    owner_id = model.get_userid_by_name(username)
     print "OWNER_ID", owner_id
     author_id = session.get("user_id")
     wall_post = request.form.get("wall_post")
@@ -56,6 +78,12 @@ def post_to_wall(username):
     print "WALL POST", wall_post
     model.submit_post(owner_id,author_id,wall_post)
     return redirect(url_for("show_user_profile", username=username))
+
+
+@app.route("/clear_session")
+def session_clear():
+    session.clear()
+    return redirect(url_for("index"))
 
 if __name__ == "__main__":
     app.run(debug = True)
--- model.py
+++ model.py
@@ -12,23 +12,44 @@ def connect_to_db():
     CONN = sqlite3.connect("thewall.db")
     DB = CONN.cursor()
 
-def get_user_by_name(username):
+def get_username_by_userid(user_id):
+    connect_to_db()
+    query = """SELECT username FROM users WHERE id = ?"""
+    DB.execute(query, (user_id, ))
+    row = DB.fetchone()
+    return row[0]
+
+def get_userid_by_name(username):
     connect_to_db()
     query = """SELECT id FROM users WHERE username = ?"""
     DB.execute(query, (username, ))
     row = DB.fetchone()
-    print "THIS IS THE ROW", row[0]
     return row[0]
+
+
+def get_username_by_author_id(author_id):
+    connect_to_db()
+    query = """SELECT users.username FROM 
+    users JOIN wall_posts ON (users.id = wall_posts.author_id)
+    WHERE author_id = ? """
+    DB.execute(query, (author_id, ))
+    row = DB.fetchone()
+    return row[0]
+
+
 
 # get_all_posts(username):
 def get_all_posts(username):
     connect_to_db()
-    query = """SELECT users.username, wall_posts.content, wall_posts.created_at  
+    query = """SELECT users.username, wall_posts.content, wall_posts.created_at,wall_posts.author_id  
     FROM users
     JOIN wall_posts ON (users.id = wall_posts.owner_id)
     WHERE username == (?)"""
     DB.execute(query, (username, ))
     rows = DB.fetchall()
+    for i in range(len(rows)):
+        rows[i] = rows[i] + (get_username_by_author_id(rows[i][3]), )
+
     return rows
 
 def submit_post(owner_id,author_id,wall_post):
@@ -39,6 +60,11 @@ def submit_post(owner_id,author_id,wall_post):
     DB.execute(query, (owner_id, author_id, now, wall_post))
     CONN.commit()
    
+def create_account(username,password):
+    connect_to_db()
+    query = """INSERT INTO users values (null, ?, ?)"""
+    DB.execute(query, (username, hash(password)))
+    CONN.commit()
 
 def authenticate(username, password):
     connect_to_db()
@@ -58,5 +84,4 @@ def authenticate(username, password):
 #        return None
 
 
-#authenticate("test", "test123") 
 
